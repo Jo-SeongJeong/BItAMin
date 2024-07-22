4f37e0fac3c23d491b06c861d04f0c1e915285b8
이재준 <sktks11@gmail.com>
2024-07-22 09:54:13 +0900

    init: spring project 생성
    Former-commit-id: 302c9d24161a034d117e0134ae4efd645d43e634

---
+++ VITAMIN/src/test/java/com/example/vitamin/VitaminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
