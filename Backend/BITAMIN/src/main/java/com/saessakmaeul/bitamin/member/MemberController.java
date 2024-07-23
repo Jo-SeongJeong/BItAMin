f4728558ac5cfa3740b56d16106eccb720749440
hyun <a94460093@gmail.com>
2024-07-23 13:20:00 +0900

    Chore: DTO response, request 분리
    Former-commit-id: f13fb9b33a89f7db58d62f52cb268af924e3a012

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/MemberController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5385b9010c7fe2a1d96b9b3463d262bd0db65c47c2d7f211a8b6ff21bc48fa79
-size 1887
+oid sha256:17e6d9ecddf1f1d4fd761c782b08a8f1c9a1776d1f4f7d9eb41f52c68d522f38
+size 1928
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/entity/Member.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:010dc893acc92b2fbcb9c1b1dd77188ab0279fa500a0244607f7cfc01092ba73
-size 931
+oid sha256:21a6bab84d9a42fcfa70a2922a628b6bda534cf1c138ddf58b15fc9f028e744d
+size 956
