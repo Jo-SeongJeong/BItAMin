dec9eff45fb6814fd43800a08d8fbb0189e3cd0a
SeongJeong <jsj046@naver.com>
2024-07-23 14:39:09 +0900

    fix: consultations 전체조회 성공
    Former-commit-id: 1581bb32b874f4d83c6a3c594e37442c0a4a3bcf

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultations/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultations/controller/ConsultationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:716c2c07f85f0549a589aa5f0b3251732f70600a80f14c734a5e7b6e09034224
-size 1519
+oid sha256:9eca2ecaa71523f4d5f55f35d240e46fc4fe91d037a683e656f9e5de627cf832
+size 1520
