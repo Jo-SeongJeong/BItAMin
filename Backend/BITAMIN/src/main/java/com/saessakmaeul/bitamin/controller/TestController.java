0b0795300a4cd5ad284bffe0e4b79146c4c0c8b2
hyun <a94460093@gmail.com>
2024-07-23 12:34:33 +0900

    feat: swagger 설정 추가, Member register 기능 추가
    Former-commit-id: 0885a9283b1c3822017454f6e7b9aa8caabbfb7a

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/controller/TestController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/controller/TestController.java
@@ -1,9 +1,7 @@
 package com.saessakmaeul.bitamin.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -15,4 +13,5 @@ public class TestController {
     public ResponseEntity<?> test() {
         return ResponseEntity.ok(Map.of("message", "ok"));
     }
+
 }
