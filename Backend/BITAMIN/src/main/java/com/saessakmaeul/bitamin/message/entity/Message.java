36606b5718c1a719401175f7019a71252c393e65
이재준 <sktks11@gmail.com>
2024-07-25 16:45:36 +0900

    feat: #19-메시지 등록 구현 완료
    Former-commit-id: 72b9f50f19291ff6a886199e98667e97c3cd512f

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/entity/Message.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/entity/Message.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12c4c133272cf2c6780a40ec71c040ee70b6e9fce1a5cda18c29c351c759e13a
-size 908
+oid sha256:d5537784aff4495c8ecdd28c39ab20ab7d85be4077e7e9c614384d8eebde4a2e
+size 961
