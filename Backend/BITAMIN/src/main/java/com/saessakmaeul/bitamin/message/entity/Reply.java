c4ce4667b21d3ea146c17c5a0159b2fc153b0b26
이재준 <sktks11@gmail.com>
2024-07-25 15:57:49 +0900

    fix: #17-메시지 디테일 삭제 메시지 처리 수정 완료
    Former-commit-id: ec30316a70570e2183ef7b9ecba1b24cea1b224e

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/entity/Reply.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/entity/Reply.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aef80f59fba874e47d1f7542095cdc6fca5b3a064774f331fa2a6f91aa540592
-size 733
+oid sha256:158429ade119251b33cb26164f94c9e0e78c3b65faadcb9f727c165ead5b609c
+size 729
