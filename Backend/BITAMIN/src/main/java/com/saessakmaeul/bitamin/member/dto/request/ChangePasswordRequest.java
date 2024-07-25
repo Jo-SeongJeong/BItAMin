f88d7dd7e9665ec68080792f9dc0002611a1558e
hyun <a94460093@gmail.com>
2024-07-25 13:51:40 +0900

    refactor: 회원 비밀번호 변경 기능 수정
    Former-commit-id: 6412897f880028244814b6b79864853601ea1b4f

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/dto/request/ChangePasswordRequest.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/dto/request/ChangePasswordRequest.java
@@ -6,5 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ChangePasswordRequest {
+    private String currentPassword;
     private String newPassword;
 }
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90548e7d1cf1bddcc9119233b6a305a6162e0f16420c440c3450a4df82d87ec9
-size 5900
+oid sha256:ef71f9149d92337178cb08fc996e409c8d1e0238eeef5c60d9616e3cb83e4559
+size 6284
