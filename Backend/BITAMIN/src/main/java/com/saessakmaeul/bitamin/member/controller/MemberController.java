1460bfa4c85600e3e96c967f1eed5bb4fb0d7992
hyun <a94460093@gmail.com>
2024-07-25 10:36:37 +0900

    feat: 회원탈퇴 기능 추가
    Former-commit-id: 36bdaebcf0a80c38ab1aa2628e84c2764fd29837

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/MemberController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28f9fab5ca897a52f5780a72ca8e99479a678c9dc86a32580cb96dc1979ccad0
-size 4575
+oid sha256:b2a4061ad6bc33847635125825751d2c22e79080288bdeaf438a85ac88227e4d
+size 5804
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d9930ae04316fb54bd8666add586d471d228f0bd18099d55dcecfc08e0f704b
-size 518
+oid sha256:4c268a645a1874e53cd73d303230547bc6fa4b7a579724bb5caad13d40498d4e
+size 556
