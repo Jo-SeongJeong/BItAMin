6ef539f785011fd25ae0f413d35fcf8db023af6f
이재준 <sktks11@gmail.com>
2024-07-25 10:19:08 +0900

    feat: #10-controller생성
    Former-commit-id: 3efe09f397514f2cba81747378083f81e85d4254

---
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/controller/MessageController.java
@@ -0,0 +1,10 @@
+package com.saessakmaeul.bitamin.message.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+    
+}
