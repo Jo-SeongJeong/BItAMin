778e25d646f743f47b604b67084e7c81f9d8e5a7
jaejunlee11 <sktks11@naver.com>
2024-07-27 23:56:09 +0900

    fix: #22-신고 기능 controller 생성
    Former-commit-id: 82474e846a946df8bb49427839cd7f072c3110b7

---
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
@@ -0,0 +1,10 @@
+package com.saessakmaeul.bitamin.complaint.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/complaints")
+public class ComplaintController {
+
+}
