d301ac1033fea064e2c542ce99e91f175ab2fa21
jaejunlee11 <sktks11@naver.com>
2024-07-28 02:03:22 +0900

    feat: #22-신고 리스트 조회 기능 구현 완료
    Former-commit-id: 506ce3377ea95e20e010759cb2fc6718d614a0d5

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
@@ -1,10 +1,3 @@
-package com.saessakmaeul.bitamin.complaint.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/complaints")
-public class ComplaintController {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:77840ec4893d60508b872545524e10e48dfe29cd26fb1d083f3f619f9b4b9bc9
+size 1317
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/dto/responseDto/ComplaintSimpleResponse.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/dto/responseDto/ComplaintSimpleResponse.java
@@ -1,4 +1,18 @@
 package com.saessakmaeul.bitamin.complaint.dto.responseDto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
 public class ComplaintSimpleResponse {
+    private long id;
+
+    private String complainantNickname;
+
+    private  String respondentNickname;
+
+    private LocalDateTime sendDate;
 }
