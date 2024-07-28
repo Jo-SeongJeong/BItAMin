5ce579ab3d8e2ae91e3d8b9f692b6047e7ab0674
jaejunlee11 <sktks11@naver.com>
2024-07-29 01:08:45 +0900

    feat: #26- message service 테스트 완료
    Former-commit-id: 7347c83003a03443dfbaf6a1cf631178f45be59c

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/dto/requestDto/ReplyRegistRequest.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/message/dto/requestDto/ReplyRegistRequest.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ReplyRegistRequest {
     private String content;
 }
---
+++ Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/message/MessageTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6a013f3f53e698b471e8fc28d3609d690043860fe159cbea2ced89f1b339b20
+size 18114
