5b3630dc8572f469af95cd73dca8c4419c646eec
jaejunlee11 <sktks11@naver.com>
2024-07-28 13:50:24 +0900

    feat: #24-신고 등록 dto 구현 완료
    Former-commit-id: b1aec8d9f8b64600ec758782a0f21b7dae732f95

---
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/dto/requestDto/ComplaintRegistRequest.java
@@ -0,0 +1,14 @@
+package com.saessakmaeul.bitamin.complaint.dto.requestDto;
+
+import lombok.Getter;
+
+@Getter
+public class ComplaintRegistRequest {
+    private long respondentId;
+
+    private int category;
+
+    private int content;
+
+    private int type;
+}
