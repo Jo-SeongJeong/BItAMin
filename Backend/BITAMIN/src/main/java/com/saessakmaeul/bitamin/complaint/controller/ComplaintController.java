9031ccd91e09b03a9c7c76312c6121e8708131ed
jaejunlee11 <sktks11@naver.com>
2024-07-28 13:40:46 +0900

    feat: #23-신고 디테일 조회 기능 구현 완료
    Former-commit-id: 3ee297361470cb4c51717d4bdc1c1e9f47dea25a

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77840ec4893d60508b872545524e10e48dfe29cd26fb1d083f3f619f9b4b9bc9
-size 1317
+oid sha256:959b25e90698fe9f691f0cbd0ab536dc25604f708e78b86f5f5ba7cd2a9e2752
+size 1729
