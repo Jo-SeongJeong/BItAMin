0ed73ee7fc7a974fd33f0a9075bf774b34e3055b
hyun <a94460093@gmail.com>
2024-07-29 15:18:07 +0900

    refactor: access token, refresh token 저장 변경
    Former-commit-id: 55ead69af5f3d14de9009a1b2f7b5facb6c7d726

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02d10c92cb1e214bcaf5184a9cd0fa81cad22d9f629da95b9ae16de298f3085d
-size 7239
+oid sha256:61c02f05ba4291028f83414492c22a610453cc0117988a0e184d963a7a83a2d8
+size 7236
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/util/JwtUtil.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/util/JwtUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69637d374d4aeecc9e99d648ce13b1453b1f0de8125bb213a2167e9347900607
-size 4464
+oid sha256:73e2461fe4980d90976f78d86eb4c2fb09f3827857dd3de6bfeb64c0ff4e67ff
+size 4597
