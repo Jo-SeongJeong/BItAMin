e2ff3ec9646ee86cdf3c7843d92e7726d09a7210
SeongJeong <jsj046@naver.com>
2024-07-30 17:48:03 +0900

    feat: openvidu session 생성
    Former-commit-id: 776e48a8a748e5333de570161aa855df4a9745d3

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultations/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultations/controller/ConsultationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:667f361bc05a4a8f3b9b63069ecc19a5984f3ca0199df296d2396fe34e943897
-size 6623
+oid sha256:5204d95764465cc9cc38f88d06008d59fc7e0b6d8516dbfaf5c259c3269dbdee
+size 8896
