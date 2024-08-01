e973b1882e936eccd61705ef011e3eff14b11cee
SeongJeong <jsj046@naver.com>
2024-08-01 17:08:46 +0900

    feat: consultation selectAll() 단위 테스트 기능
    Former-commit-id: e35d5064ab6c05db07b27f5c71d12bb88d3c37dd

--- Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/consultation/ConsultationServiceTest.java
+++ Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/consultation/ConsultationServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a5e9d4b7d7bdd92a0f30aa9d5989c3b7cad721ceb6117ec9c4df7e605ad1e45
-size 9208
+oid sha256:60b1e251222a9700074f151dc77da2aa78682d497d262d73f9cca09afaae8b30
+size 11696
