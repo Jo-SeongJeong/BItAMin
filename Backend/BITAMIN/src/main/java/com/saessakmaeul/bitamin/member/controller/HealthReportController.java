52d4f347dffbba6bd254e4c88f3cc84f30a97770
hyun <a94460093@gmail.com>
2024-08-01 16:58:48 +0900

    refactor: healthReport 리팩토링
    Former-commit-id: f78c15802042794a539e670600a0c0a9536775af

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/HealthReportController.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f526b5ebd6bef7cfe875d63e07139e9574002d35faae277625816421f6c0177e
-size 3125
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88ca70ea30b66b6ea81e06d527bbf1a01c87f0b142f1d6ec7a12df6610d8452c
-size 10940
+oid sha256:9b5a4aa81b5fdf392c26d7685c0128d9889ec2872869fbe5cc695014d89142e7
+size 12547
