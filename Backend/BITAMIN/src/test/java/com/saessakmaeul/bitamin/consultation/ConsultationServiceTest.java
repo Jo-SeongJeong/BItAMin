632503372811f06bf3e22998558e57b449e63205
SeongJeong <jsj046@naver.com>
2024-08-02 12:34:54 +0900

    test: 상담 방 참여
    Former-commit-id: eb064265d481ef5e4bf0b2ccec0714132d40b662

--- Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/consultation/ConsultationServiceTest.java
+++ Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/consultation/ConsultationServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4c046feb616a70e8b793ae0dee71475d46e1420cf9d7957e3d0d0500c85249
-size 13385
+oid sha256:72de9f3ee7ef5492d26a1ca56c45435b4ce49dca6b3442305ee984afa50447bc
+size 16594
