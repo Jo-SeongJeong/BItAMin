22ec1ddd1b5a9b50faf6a1ffb88e89acf8426110
hyun <a94460093@gmail.com>
2024-08-02 09:54:33 +0900

    refactor: 리팩토링
    Former-commit-id: e4c8623bdac83143890553bd16eba13ddd193ce9

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/MemberController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a226cd417b1e84d1e8b8f6b3421ad9830e13ed06e073a6962f25a6cda99c2ce4
-size 8357
+oid sha256:dd24d6ebfb2080ed907ea3e6b1cc668715c77d62772a94e04fe75185e828d1f9
+size 10450
