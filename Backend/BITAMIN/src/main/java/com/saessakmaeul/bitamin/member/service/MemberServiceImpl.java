1cb20c24a492aa8a8ad00ebe7e3481d0a8efe4c2
hyun <a94460093@gmail.com>
2024-08-02 12:21:41 +0900

    chore: file delete
    Former-commit-id: 2e4935ea9a23679dae0853879afb44376a8c4a97

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b5a4aa81b5fdf392c26d7685c0128d9889ec2872869fbe5cc695014d89142e7
-size 12547
+oid sha256:104a924fbdb0e293fe113d0ffbeadc2f7d07714dacb68ffae16c425cb3b12b4e
+size 12207
