1a33da472421a89f0553e117037f2c9bd217169e
이재준 <sktks11@gmail.com>
2024-08-05 16:07:52 +0900

    hotfix: #51-CORS
    Former-commit-id: 4417112187b80f31ec8d4d9f48171025215fe6fa

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad34760dc90f65fa77666e36c41d3622939865b6b23e98758fc2e2bef5d3fd7d
-size 1526
+oid sha256:c86452f9a68b1cf0c0ae9e1b3888402bcc287bb41c9bd674638c2a76dd2b7955
+size 1484
