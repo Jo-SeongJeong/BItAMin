c523f015b33df9737d7f73e42c2bf5841ef7481d
SeongJeong <jsj046@naver.com>
2024-08-05 15:16:13 +0900

    refactor: Service 로직 수정
    Former-commit-id: dbe618d6c0c90878dc9ac53e9f13bbc2569f6e9a

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/service/GptService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76c2741f41238571666396b805b61a03e5896de7d39b89810ae25d09766e3ddb
-size 2596
+oid sha256:a7fec643ec13935b462e8ac24c0d9b253e5a07114a6e8e4caf523d1ceb223afd
+size 10954
