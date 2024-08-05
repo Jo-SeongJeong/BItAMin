1825c3fc4bc4904fcfc21ff4e9fed6df44a8109e
SeongJeong <jsj046@naver.com>
2024-08-05 10:49:02 +0900

    test: 타입 변경에 의한, testcode 수정
    Former-commit-id: bfca89cca7d4ee4b7dfae877f95b9beeb26de0bb

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78db882f18f20804e2ff818e9789fad89138ca1882136bee48adabfd72636957
-size 8984
+oid sha256:227cbb509c8cbaf3f3c740d88a86de0ca449287b3c9236d6aa72a460d04ba298
+size 8990
