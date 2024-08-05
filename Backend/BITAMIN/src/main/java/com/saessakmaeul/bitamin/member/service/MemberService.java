6b7e48dc4965326481dc203fbaf23993d1495ba3
hyun <a94460093@gmail.com>
2024-08-05 12:37:44 +0900

    refactor: register, updateMember 한번에 모두 저장으로 수정, @Transactional 어노테이션 사용 수정
    Former-commit-id: fcf7859b23e9aa7ca9031cca2ed0eca2c4bef7a2

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6849852fba66c62a5f985c00013c80c6208df7a791024149e0ba702a37e69bf
-size 14503
+oid sha256:66bae12f592a565c29053c712920b643ac476e9b7b2d16f2cee91410b3a00b5b
+size 14176
