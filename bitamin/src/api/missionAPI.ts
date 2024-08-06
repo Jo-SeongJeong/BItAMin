b0d6d48f597ad035864ae285f97eafde339f6304
hyun <a94460093@gmail.com>
2024-08-06 15:29:03 +0900

    page: 1-2 메인 로그인 후 페이지 api 틀
    Former-commit-id: c8b21a372390a0804b5984c8bff173a2c154f165

---
+++ bitamin/src/api/missionAPI.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2b40e0b42a968bcee76adf86813c89f1a70b8b667677e4429b0efa00cc85064
+size 747
--- bitamin/src/stories/pages/main/MainPage.tsx
+++ bitamin/src/stories/pages/main/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:331fe67e0b3530a6d5f14aeb188816424b1774e14f9822c8f35c5c60188b97ad
-size 8641
+oid sha256:a5e2d9492bedb99cd2113d7f5d1d946cd49cda21089c24775c6cfe06ab9225fd
+size 11085
