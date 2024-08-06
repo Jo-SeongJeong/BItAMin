4f28556429619bd0577b78e14c6c3dc8ad535fc9
최승탁 <tmdxkr5@hanmail.net>
2024-08-06 16:47:49 +0900

    page/4-1-consultationlistpage: 리스트 구현 완료
    Former-commit-id: bd75737c002ae0200ecba59dceb4e388bcaf3b3c

---
+++ bitamin/src/api/consultationAPI.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:231186ac6f121750d0b158187b7567203b066b72936c01f12c2ec042d621eb25
+size 535
--- bitamin/src/stories/pages/counsultation/ConsultationListPage.tsx
+++ bitamin/src/stories/pages/counsultation/ConsultationListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e65fdaf53d3115705b46b121dd369e6dfdb216dc898d7100eea5c9391d3116d
-size 6535
+oid sha256:a8351d80fec021fe42143446dc9b74fdf2c4ed0a9b2dd0902792a12a878402bd
+size 6630
