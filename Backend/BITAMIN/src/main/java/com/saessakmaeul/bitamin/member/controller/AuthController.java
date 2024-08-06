4b1491b239edca855ee007251fcc23ae5476c346
hyun <a94460093@gmail.com>
2024-08-06 09:49:11 +0900

    refactor: test용 출력 코드 삭제
    Former-commit-id: 0cc325e9080e8cc14e17e97ef832fdc41f4bc6df

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:280c9467388978e983679dab181209feab93142e5bdd45509df5a5eb179c28da
-size 6039
+oid sha256:178117812642cafa753eb85838826d6d85f7b9f4cf96492aa2cadc4507488fa4
+size 5910
