eded0a7a466fdda8407a507a4cc203925b580a2f
SeongJeong <jsj046@naver.com>
2024-08-07 12:45:17 +0900

    refactor: participant 관계 부여
    Former-commit-id: 82ad7f5547a135c4788361d7a8edc6ce10ad22ad

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9946dab841dc195d2ee70c64e9ee51321b0aa6a06cba3e6d5b06e40234bb8d7f
+oid sha256:f49828dd526c7c5f1c91fe3bec1a1fa831f0b2d8b2de6ceeb07c2a4382c808f3
 size 9879
