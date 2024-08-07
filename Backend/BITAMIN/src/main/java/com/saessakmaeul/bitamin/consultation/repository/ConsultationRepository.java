715e298743256d5176c8b1fcfbdb723baf98e846
SeongJeong <jsj046@naver.com>
2024-08-07 09:16:37 +0900

    fix: 오래된 상담 제거 스케줄링 쿼리
    Former-commit-id: 47cae751b640883afc58ebc3218b612d8482b0ec

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/repository/ConsultationRepository.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/repository/ConsultationRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8853585883825e02ae5db597e57c194c99f9602b676ecc69fb3405c63e6e71f
-size 1931
+oid sha256:3914c1a5c01a7143cec96223a02bc8cdf8a5cc479ba86bd9fc71874b44e77f5e
+size 1932
