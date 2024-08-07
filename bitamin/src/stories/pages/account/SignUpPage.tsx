d6ab4aa3eef2e0a8ae1584c02313add8c2f708be
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-08 00:35:53 +0900

    자가검진 점수 결과
    Former-commit-id: 3fb88b8f3e98fa6bcb39ed20c9b97a7b90a87c71

--- bitamin/src/stories/pages/account/SignUpPage.tsx
+++ bitamin/src/stories/pages/account/SignUpPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19ff3beb935d5ee0a68bfa7a98b4d22c3e968c50bee5cc9eb03b171a38fac852
-size 21981
+oid sha256:730ff23ffaa6e58aab10f49bb7cf86ca860772ee0562c6efd7c9e9b5157cf182
+size 21475
--- bitamin/src/stories/pages/account/SurveyPage.tsx
+++ bitamin/src/stories/pages/account/SurveyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a5539dff23d988d0ffa1b0905b1d002a9096e1c50ee6f02761dceed2cb96c96
-size 31063
+oid sha256:493e1c8c75f6b8bca2a1df7412b8254a1a82b2e456daf3969e823887d3d80389
+size 8389
