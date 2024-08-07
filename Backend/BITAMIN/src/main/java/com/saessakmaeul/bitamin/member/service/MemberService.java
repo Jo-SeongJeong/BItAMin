c28727c9ed0bd61da6d12e535863a10350d8b106
hyun <a94460093@gmail.com>
2024-08-07 13:43:14 +0900

    refactor: s3 프로필 이미지 등록, 수정
    Former-commit-id: 159d1ee7e67d76d665b2abcc4701ba6521b98438

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f0b5e3483bac385e276364d543a6a19c2557972d51f02e01cad12496ad1ee2e
-size 14856
+oid sha256:3af5d7d9912cece30cf3458b3e32bb30114d3e528dc040e4821910dd9cc80171
+size 14049
