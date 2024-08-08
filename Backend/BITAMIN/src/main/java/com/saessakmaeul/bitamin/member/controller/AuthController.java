9f9810b7bba6b72c98b5d8cc76f5491b15fa4d7d
jaejunlee11 <sktks11@naver.com>
2024-08-09 02:14:24 +0900

    feat: #76- kakao 로그인 테스트 완료
    Former-commit-id: 026552059b8cce8e06a9b316c26d7f6b9a64f944

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80f9d24546ab968481bedba78da71e43657bfc23c9db1f4da2ba2085a2aa7fd4
-size 5671
+oid sha256:9d33419d2ea151cb17c562fe3447a70219d8834fc9a604d7aae2030536faff84
+size 8775
