303aa582c0b2102174d769978d7b7a84d0a759bd
hyun <a94460093@gmail.com>
2024-08-09 00:21:11 +0900

    feat: 자가진단 7일 이내 기록 여부 확인, 최근 기록일
    Former-commit-id: 18bf815fba6c1e8c7e8b407be1e80c0b1b8d4c19

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c51f6f2808e3f26920f1243e4f930a3304d5532f9b83703ef34dd44767f127b
-size 17408
+oid sha256:33dbc6fccc30d1afaf8f9831ce82d61c1029995d294bda22591199d7f8a59e0b
+size 15134
