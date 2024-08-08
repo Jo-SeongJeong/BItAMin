409b01ebd3bfd3d4244ec3a4c63f3e26165200b1
hyun <a94460093@gmail.com>
2024-08-08 13:29:02 +0900

    page: mission 등록
    Former-commit-id: e4008ef631c0fa15a5f55c9f3d617739db1cd024

--- bitamin/src/stories/pages/mission/MissionForm.tsx
+++ bitamin/src/stories/pages/mission/MissionForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fa20168e12d9341388f99232f4a1adb15396e16681a449443772b192c048570
-size 4620
+oid sha256:46eb4cb03d93f86c0cdc0f289100dad3d44adcd9acb621fcc7a1a52a243ed0c9
+size 4672
