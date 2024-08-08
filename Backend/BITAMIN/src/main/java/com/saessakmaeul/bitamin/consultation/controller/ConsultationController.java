3f7e81019af4639c581c3e4a4d8df043659ad43f
SeongJeong <jsj046@naver.com>
2024-08-08 19:36:52 +0900

    feat: 프론트 gpt test 위한 로직
    Former-commit-id: 88db807900f8b09ad03fca8d4ca5e73df711cb1e

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd926c36ba0936a0fdfc4112aa7c90f97f8df2308a141a7ab33386c7df81d600
-size 11323
+oid sha256:b65d0764dc6133d4a2df11878b6b6fbd1a5670988034df7e83f31dbcd35dfd5d
+size 12364
