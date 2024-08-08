72f0b1195ef8a831e292a679e6e94a2bee48c537
SeongJeong <jsj046@naver.com>
2024-08-08 17:13:51 +0900

    chore: front 테스트 위한 주석처리
    Former-commit-id: 228826f81bf02e9918d65e4306cc447483e346e0

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/service/ConsultationService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/service/ConsultationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e828cde6169d62ee44fd278cfb6ea7b944b9336a2f7fdc5c9d63ce13eeaeb36f
-size 19182
+oid sha256:b4f68bbf4b1fd3e218cf78196fc261204741e55ee1657ac4b069d6157b82c2d0
+size 19198
