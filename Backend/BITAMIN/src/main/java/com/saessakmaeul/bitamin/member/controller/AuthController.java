7bec397365fbc16abb9d0a7a4b8e5ae24d5500f2
jaejunlee11 <sktks11@naver.com>
2024-08-09 03:56:45 +0900

    feat: #76- google 로그인 테스트 완료
    Former-commit-id: bc89b260511bc3ac247f954246a9b5e8fde3fad6

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d33419d2ea151cb17c562fe3447a70219d8834fc9a604d7aae2030536faff84
-size 8775
+oid sha256:df2746ef652ab9d297dc95301d52e1305eb6e215b2d91aea7424143d36d7c41a
+size 11958
