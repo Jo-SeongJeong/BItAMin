507f7b4e88df5a3937bae8081922df3e0a0be621
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-10 01:24:19 +0900

    refreshToken, accessToken 보안강화
    Former-commit-id: 80b098d325e0d994d241964e6ebc16ad85b79597

--- bitamin/src/store/useAuthStore.ts
+++ bitamin/src/store/useAuthStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11dd5ec8b4db1afb0cd3cfd20b8fb60647e839498d3f174314d01ba5c70f686f
-size 1712
+oid sha256:0ed5c98af37b7daa6c70365c6cb6d22d58d57bd12141315cfe8619bdeac08958
+size 2274
