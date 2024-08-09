f4cf376a6a0130cf9f0dfa789498e3209249b9eb
이재준 <sktks11@gmail.com>
2024-08-09 10:22:57 +0900

    feat: #76-naver 로그인 구현 완료
    Former-commit-id: 48b32d6c91f1e496b3868513cf42295a9753be18

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df2746ef652ab9d297dc95301d52e1305eb6e215b2d91aea7424143d36d7c41a
-size 11958
+oid sha256:f36242b57c975ffa43046542d58b366f51381cab9ac646e23961e0f62174fb31
+size 15014
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:706c97e7378e2c5b452cdcad3281694e5209d40477635318dd5049d30f0e1767
-size 24439
+oid sha256:4646b57e95bf06c74fbf3066b13c3df843fae7fa6fb7a4b1980e1f3fa0ddb5e2
+size 27760
