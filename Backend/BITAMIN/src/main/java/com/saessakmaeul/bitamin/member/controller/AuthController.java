6dc76d400a6eb19295b19ef12ae1f622321d7791
이재준 <sktks11@gmail.com>
2024-08-09 17:41:45 +0900

    release: 배포 url 설정
    Former-commit-id: 6758e85ba32519ffe1cf8527e5cd60d55dbd2f67

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f36242b57c975ffa43046542d58b366f51381cab9ac646e23961e0f62174fb31
-size 15014
+oid sha256:2c09e4b4acb18c2097f9366170ae715ffb1efd3ab18d4eeb91c9197a6b1aae60
+size 14484
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4646b57e95bf06c74fbf3066b13c3df843fae7fa6fb7a4b1980e1f3fa0ddb5e2
-size 27760
+oid sha256:68acb220b8463921ef04bf4de870d7a24c84d33d70c81eba6dcbeeb7d27a1735
+size 27607
