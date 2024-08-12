bef14242c059dd256d1b7bfa7e1efca054327515
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-13 02:30:39 +0900

    건강UP 리스트 페이지, 마이페이지 css 변경
    Former-commit-id: c1553db05008f4c1f2bd820ffedb47de6a0a13ea

--- bitamin/src/stories/pages/account/MyPage.tsx
+++ bitamin/src/stories/pages/account/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:569e8345aff46afb2cc64be9e2566d0ab3cba9c8ce1552e97ee6914771b44dad
-size 19198
+oid sha256:0c0543a8caf27e393578160befce0b9d43301e02004d368fa03486a817fc6101
+size 19130
