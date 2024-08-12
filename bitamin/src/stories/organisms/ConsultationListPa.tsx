cbdc86185c143807bec359e469cd2fcbcd257362
최승탁 <tmdxkr5@hanmail.net>
2024-08-13 07:10:14 +0900

    refactor: build 오류
    Former-commit-id: 20e34d5366bcaa5364bdfd597760a63b6dafcda3

--- bitamin/src/stories/organisms/ConsultationListPa.tsx
+++ bitamin/src/stories/organisms/ConsultationListPa.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5edbf117a45da7b563d8f321c1c5917432f7b41281ac2c34b2c5cb6c7bf2560
-size 5812
+oid sha256:0fec20b827c9ede016d85ae5f251f1549e1698e4ba9bcb82975ddd190a66c3dd
+size 5813
--- bitamin/src/stories/pages/counsultation/ConsultationListPage.tsx
+++ bitamin/src/stories/pages/counsultation/ConsultationListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:137b7f3d035d33fdfd785deaf944d3a3adeccefd1813970ffd049a58a7540db9
-size 6729
+oid sha256:da4600e4129a6bd61f683982fbb4e4b332b019bc380d5823530b94f366fdb38d
+size 6667
