d2e5c2ce9d52ec2583d13d63a61f1188166cae5f
jaejunlee11 <sktks11@naver.com>
2024-08-12 22:45:04 +0900

    feat: #86-modal
    Former-commit-id: 0dd342582e10ba0d7f8df9f989ae54fb2fcff1f8

--- bitamin/src/stories/organisms/Modal.tsx
+++ bitamin/src/stories/organisms/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ed7ab87c936da2871bd6b6b7586539de32afa59360fcc3f789bf3b658c11df
-size 2678
+oid sha256:07ef1a66cf51014119d32c588323f2ba0f37538b0fd186e8059e75f02b81e8dd
+size 4237
