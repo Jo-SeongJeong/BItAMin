76fc14817cc6aee6deea866a1269e4e15f203a24
최승탁 <tmdxkr5@hanmail.net>
2024-08-12 09:20:25 +0900

    feat: 모달 예시 생성
    Former-commit-id: 267ce66e73af3a9e6064d2a8d0c921a40c644f78

--- bitamin/src/App.tsx
+++ bitamin/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eeca23930a71f1daf155604d6f5ef8c3f19633822b580f1103d16fd1a88fa96
-size 1244
+oid sha256:e05746f8a2d0965b2a1decbd06addec2208f892d1df811fe2674ef2856d7eb91
+size 1254
--- bitamin/src/stories/pages/main/MainPage.tsx
+++ bitamin/src/stories/pages/main/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea791fa59f712c29f11fab7fe818a650703f95fc9e587ce5b48a2f429c1c0c77
-size 10372
+oid sha256:1bc17964d939a6a289b752b540eca415f634076ca79a162ee98012c2cf5cc520
+size 10380
