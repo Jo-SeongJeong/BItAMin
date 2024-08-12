f0740f29a9ed8e754478c8d41063d27592de70bf
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-12 22:53:25 +0900

    마이페이지 css 교정
    Former-commit-id: 24dafd18712c99f2a36decd57dda30e099b9597d

--- bitamin/src/stories/pages/account/MyPage.tsx
+++ bitamin/src/stories/pages/account/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:710906420d876f7dad157f4b031a8ab4aef216d1778df064b5e9635386cffaba
-size 18841
+oid sha256:569e8345aff46afb2cc64be9e2566d0ab3cba9c8ce1552e97ee6914771b44dad
+size 19198
