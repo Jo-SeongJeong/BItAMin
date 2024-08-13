1eaa088e751979797659c9cfb7ee7814ff73e02b
최승탁 <tmdxkr5@hanmail.net>
2024-08-13 15:32:46 +0900

    Revert "page/4-2-ocnsultationpage: 화상회의 2차 마무리"
    This reverts commit e08737e5851ab615f37d2fec16aa6927d31325fe [formerly 2d4bd9537ee293744c76153ded639006c8fa80cb].
    
    
    Former-commit-id: 10117f125817098e6f1f46d23bfc5e2a082ee795

--- bitamin/src/stories/organisms/ConsultationListPa.tsx
+++ bitamin/src/stories/organisms/ConsultationListPa.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:417d5188cd4261a119a168b0ffd641215645581c6b16738ae644d909f9cc8022
-size 7404
+oid sha256:0fec20b827c9ede016d85ae5f251f1549e1698e4ba9bcb82975ddd190a66c3dd
+size 5813
