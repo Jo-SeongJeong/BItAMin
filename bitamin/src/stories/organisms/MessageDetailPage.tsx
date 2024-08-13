d9da41c3456163d20f010c87778cb5d147ff0671
jaejunlee11 <sktks11@naver.com>
2024-08-13 17:39:44 +0900

    page: #88-쪽지 삭제 모달, 쪽지 전송 모달 추가 완료
    Former-commit-id: 3b9fb951af7fd2b0ec3f8101b82ae67abe88a9b2

--- bitamin/src/stories/organisms/MessageDetailPage.tsx
+++ bitamin/src/stories/organisms/MessageDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a74a24548c852c99f6e4d95cfbad17915755ca153a6674a3994bafad3f7bf94b
-size 5569
+oid sha256:d0c14e11a309074a2943a22e4c8c06918b873931da8600a874d340cd98bfb4bb
+size 6126
--- bitamin/src/stories/organisms/MessageListPage.tsx
+++ bitamin/src/stories/organisms/MessageListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb3750208d7822b1e324386b203bbe17a15deb071bebbddbb9b89e556a47f2da
-size 2394
+oid sha256:8a3c301d9f9acd72a112f9f93066d9b9ee3a8125d4d910ada764c6db5a62bdce
+size 3163
