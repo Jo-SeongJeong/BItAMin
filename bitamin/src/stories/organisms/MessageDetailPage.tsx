e3cda9269dcaf0b8b147d5447054579080af67b4
jaejunlee11 <sktks11@naver.com>
2024-08-13 17:46:40 +0900

    page: #88-답장 삭제 모달
    Former-commit-id: c743fcfdd4913c25a5450d14317d634c87472068

--- bitamin/src/stories/organisms/MessageDetailPage.tsx
+++ bitamin/src/stories/organisms/MessageDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0c14e11a309074a2943a22e4c8c06918b873931da8600a874d340cd98bfb4bb
-size 6126
+oid sha256:ee429ad3be7d7742299f868158a9d836ebb68752db2b07de2f895db10c68501f
+size 6681
