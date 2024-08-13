cbcece22d24702291b4399c6da2adb68e74d2f18
최승탁 <tmdxkr5@hanmail.net>
2024-08-13 11:07:08 +0900

    refactor: stt 구현 완료
    Former-commit-id: eef23bfb88e4e28cba53734ef356522784380c3b

--- bitamin/src/store/useChatStore.ts
+++ bitamin/src/store/useChatStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:591b7f15203b593047d92be7138d69c9376713d245fda13c53210237cafde704
-size 2628
+oid sha256:b35cdeaf4a5e927af658c358f775c572dc6847a0939cb7acff432eba64d12565
+size 3075
--- bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
+++ bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2409a4a5f42fae938a4f01a5352fbe5c86b1ddfdd8fd9eb527f756e75d067432
-size 22941
+oid sha256:71c06c9c7523868140328355959d606678259ee28dfc6d92aac6c2f6c64b8668
+size 23099
