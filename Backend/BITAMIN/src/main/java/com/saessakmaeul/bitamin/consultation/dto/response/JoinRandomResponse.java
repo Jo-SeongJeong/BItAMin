48bb1e36a90288ccd6d8f3e288abeebe1cc7a488
SeongJeong <jsj046@naver.com>
2024-08-13 12:49:31 +0900

    refactor: profileKey 삭제에 따른 consultation 코드 정리
    Former-commit-id: aa3059a5084e75ea63a1d9ee7dea438535b7df1a

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/dto/response/JoinRandomResponse.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/dto/response/JoinRandomResponse.java
@@ -15,6 +15,5 @@ public class JoinRandomResponse {
     private Long id;
     private Long memberId;
     private String memberNickname;
-    private String profileKey;
     private String profileUrl;
 }
