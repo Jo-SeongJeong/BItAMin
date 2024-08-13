c7fcc3faed13903bd8915313101805e58d52ae1b
최승탁 <tmdxkr5@hanmail.net>
2024-08-13 17:01:39 +0900

    page/6-message: 메시지 css 적용
    Former-commit-id: d5f32d71b1af4ec8c23cc2efc5e62bbf7a6f47cc

--- bitamin/src/stories/organisms/MessageDetailPage.tsx
+++ bitamin/src/stories/organisms/MessageDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bff0e36983e7cfc1ee8e94c6ec915f1c5ecd332c89ce6e5a17ee4b5de34bbd9
-size 5099
+oid sha256:a74a24548c852c99f6e4d95cfbad17915755ca153a6674a3994bafad3f7bf94b
+size 5569
