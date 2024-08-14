479c1d2d09b69c30c197bc8b997c40c163e81270
최승탁 <tmdxkr5@hanmail.net>
2024-08-14 09:18:19 +0900

    refactor: design 수정
    Former-commit-id: 27b1bdf178616f7be5704d09a006bc6da006a507

--- bitamin/src/stories/organisms/MessageListPage.tsx
+++ bitamin/src/stories/organisms/MessageListPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9057c75643e27c5c9dd0e12186dddc4a287744c2dc2fc0aaff57eaf584e3c05d
-size 4762
+oid sha256:97e2034d3090414afa6b245b4552055b2f77bd1e35dad4d5a019290ece604007
+size 5305
