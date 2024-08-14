5d976fa13b8a0f964d4d279401925e3c7e48237a
최승탁 <tmdxkr5@hanmail.net>
2024-08-14 13:54:10 +0900

    hotfix: build 에러 수정 6차 수정 Former-commit-id: 88d3faa622fea43a80c3d817a458d8ec89f4866a

--- bitamin/src/stories/pages/healthup/HealthUpPage.tsx
+++ bitamin/src/stories/pages/healthup/HealthUpPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d35af2cc74282280133923b56591614f114be043ea350c454ece38619a057e9
-size 9274
+oid sha256:ee3933919492bab0ab47afb8ce484b591bd56a924141a35792dd8bb16e0e959e
+size 9272
