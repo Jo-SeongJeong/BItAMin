af6bbcbb17dcb26166d99ef01ac4bc2fee80dd32
jaejunlee11 <sktks11@naver.com>
2024-08-14 19:54:18 +0900

    회원 정보 수정 모달 오류 수정
    Former-commit-id: 1b49a8785db220884cd043ee802ec955937c433e

--- bitamin/src/stories/pages/account/MyPage.tsx
+++ bitamin/src/stories/pages/account/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5a1669a0e5247b6db4e18a446a9cffcf9188c3fe81d421f518656cea27a3791
-size 20352
+oid sha256:af0b91b5f36291c1e78ca54f5f0464a7daaeff35cce901dfbe67f64167616730
+size 20354
