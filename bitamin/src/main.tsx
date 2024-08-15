3e7053608ca0907c0dd210c87a796a70e7f5dbad
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-15 19:29:02 +0900

    globalcss 적용
    Former-commit-id: f35b66f5fc4d2e0fc04381de96c705997eddcd94

--- bitamin/src/main.tsx
+++ bitamin/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'index.css'
 import App from 'App.tsx'
-
+import './styles/utils/global.css'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(<App />)
