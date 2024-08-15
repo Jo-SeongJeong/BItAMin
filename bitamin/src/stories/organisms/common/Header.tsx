103f47264b3cea9f854f0a765a5bc875483e5e13
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-16 03:05:37 +0900

    로그인전페이지 수정
    Former-commit-id: fcad528420b98bba4c232c0555328a4a94f61485

--- bitamin/src/stories/organisms/common/Header.tsx
+++ bitamin/src/stories/organisms/common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71a81d959d96d612bda2a34334ac1104f7a06772da67afcc1d58531874c7508c
-size 9225
+oid sha256:f017333ea6ffcaee40aef9b83ea191484f719bf307813236cac0aed95bed93cb
+size 9227
