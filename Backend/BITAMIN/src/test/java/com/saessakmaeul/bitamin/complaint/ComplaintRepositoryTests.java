dbada53578fade25561e9b9b58855109259770de
jaejunlee11 <sktks11@naver.com>
2024-08-16 03:44:43 +0900

    fix: 소셜 로그인 배포용 url로 변경
    Former-commit-id: 8d5e47f0f4ca9f6dd387a78d9dbb94264303c41d

--- Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/complaint/ComplaintRepositoryTests.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6c99b1934380c13faea76a9bc638369921e7464bb95c58bf66490c24f72ae411
-size 7671
--- Backend/BITAMIN/src/test/java/com/saessakmaeul/bitamin/complaint/ComplaintServiceTests.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:00fc0e406615569208d56d84ba69be11b8b691c82cd2987ef926fe8de8c2e115
-size 13622
