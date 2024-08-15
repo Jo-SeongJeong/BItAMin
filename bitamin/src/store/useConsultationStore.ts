595bf07e6fafaaa6cc4e01f827e957fb53d92aa6
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 18:14:51 +0900

    feat: 기본 websocket 설정
    Former-commit-id: 46f061a72e0629527cc07a6dfd6d32e3d2ff73a9

--- bitamin/src/store/useConsultationStore.ts
+++ bitamin/src/store/useConsultationStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe05a6c82272efbb67195826294510413e11cf2ba26bf0a573795e2c1733df6e
-size 6162
+oid sha256:f55f2aaa9e55c1c7d91e10e64134812a4c1a906d9c4e6076970320a9fe368793
+size 6877
