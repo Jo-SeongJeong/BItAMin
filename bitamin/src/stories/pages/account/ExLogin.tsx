06adf9e88f73de7120a4bca7ed0375fd81a60916
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-15 16:38:22 +0900

    로그인 전 페이지
    Former-commit-id: 597b34743f7e18e438589509f3871228a5e81df2

--- bitamin/src/stories/pages/account/ExLogin.tsx
+++ bitamin/src/stories/pages/account/ExLogin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:804a928b4db005daedabe6d6faeea40f2e25df20f68868ce7f86a378f0b4a367
-size 1817
+oid sha256:c4258be325516933c26100d384de7e0730e263023fe4265c4714a96d374ab7f7
+size 3538
