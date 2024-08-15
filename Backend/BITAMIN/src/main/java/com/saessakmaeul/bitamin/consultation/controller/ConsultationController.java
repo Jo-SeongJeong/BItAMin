c08aad4c61fdd3b1426092b91626a13021bbe9d7
SeongJeong <jsj046@naver.com>
2024-08-16 04:58:15 +0900

    refactor: gpt 테스트 코드 제거
    Former-commit-id: 83c44a125528770adc94ab2dc4f6f6d6c069f68f

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultation/controller/ConsultationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9352bcb023015e8395184b44ea50a29cd9339eba474348f4f3da38cbf80e97cf
-size 13657
+oid sha256:f077b2a741a556cb674c67fc42547fc5030fc2769ad3301117c606c6c982950b
+size 12616
