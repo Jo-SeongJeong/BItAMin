4c5b4222db8d17e50b46fb2747d4d7985b9a833d
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 16:29:41 +0900

    refactor: 방 css를 귀한 기초 api 설계
    Former-commit-id: 85c743b66be466214e1a2405aa29d960b84b9f75

--- bitamin/src/api/consultationAPI.ts
+++ bitamin/src/api/consultationAPI.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b197a33890e5a55f48798f5fc5f1dd52037149a2d2a47c82e56071b342ea509
-size 2743
+oid sha256:e53657791f267165df1bd32b83a644d32c8d4067af1b63470ad1b6ae7d720ed6
+size 3061
