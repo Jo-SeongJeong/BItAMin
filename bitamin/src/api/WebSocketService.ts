36dc5c702d9b9d51c9b6f1b57a4daf4b5d5cf72d
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 20:46:23 +0900

    feat: websocket 설정
    Former-commit-id: 32662079875b7e80029f499b86965dce238ecae1

--- bitamin/src/api/WebSocketService.ts
+++ bitamin/src/api/WebSocketService.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0868ba80c133cfaa973d4d50fa8781a1f92621530ed5e5ee1df0de71195c1749
-size 1931
+oid sha256:7dd8a69bf01b866b134f53d3575dbc4320f651742cf4b0488bbcc68c6b74032d
+size 1962
