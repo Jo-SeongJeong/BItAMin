788e9e4999d6e5a97096409ceeeadc9c373af814
SeongJeong <jsj046@naver.com>
2024-08-16 00:16:50 +0900

    fix: gpt 소켓 boradcast
    Former-commit-id: f6bddc66bbcffb826770a210b0298df353eab896

--- bitamin/src/api/consultationAPI.ts
+++ bitamin/src/api/consultationAPI.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ea08e0616c37abb69d85748d3830a84235c0bfd6a8da9b619c2d03c0811cb19
-size 3716
+oid sha256:79a332c0cc904349b361af6f69ffd51452cd91b914569d508cd248a57128638b
+size 3734
