6e0b329ee3cffac6eef193397c63ec7b5d2e665f
hyun <a94460093@gmail.com>
2024-08-15 16:48:23 +0900

    page: myplant 식물 레벨, 날씨 배경 추가
    Former-commit-id: c28f55896fe0acb129f6df218df1b119875e1e3c

--- bitamin/src/stories/pages/mission/Missionform.tsx
+++ bitamin/src/stories/pages/mission/Missionform.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06a7895e3ae0c7378e21bbba0be51f97ec21716717744e8b8bed7dd93c881608
+oid sha256:f3a7bea4dad391bec8e6d7eb8684a02b28c87c50529ac89a561f047648c6466a
 size 7367
