4a559b7b58805a94bce605517d745e4f4eb33ffd
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-16 01:45:17 +0900

    폰트적용완료
    Former-commit-id: 8a42fa7fb93636c756184cb217413df9a33726bb

--- bitamin/src/stories/organisms/common/Header.tsx
+++ bitamin/src/stories/organisms/common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acf58b35846c7177a42d9b52bab35ddd83fc5e3cfb190496b4370695ed90e1d0
-size 9224
+oid sha256:71a81d959d96d612bda2a34334ac1104f7a06772da67afcc1d58531874c7508c
+size 9225
--- bitamin/vite.config.ts
+++ bitamin/vite.config.ts
@@ -13,4 +13,5 @@ export default defineConfig({
       },
     },
   },
+  assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2', '**/*.eot'],
 })
