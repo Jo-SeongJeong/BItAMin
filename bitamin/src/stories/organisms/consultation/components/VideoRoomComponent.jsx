747bea1a83c0ea0b04f2cc4a74bda27f5eb34f21
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 15:43:30 +0900

    test header sidebar footer
    Former-commit-id: 40b6de1d2e9d3ffddb9089358483b9b31afa2237

--- bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
+++ bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84342d80951cfabccbc6909394a49f2f3bb299313651cd7bad3096fe87b7bc2a
-size 23537
+oid sha256:c8ec14f19aeb23699d3496f390974e1c4d523b123dbb06907765605520d8113d
+size 24698
