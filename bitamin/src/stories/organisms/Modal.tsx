6a1adbf6670ed37cdae21df76b53c44c5f6ee1c5
jaejunlee11 <sktks11@naver.com>
2024-08-15 19:43:11 +0900

    hotfix:png파일 수정
    Former-commit-id: 765619ca4ee5a530a5ae287b7e112681e99a6522

--- bitamin/src/stories/organisms/Modal.tsx
+++ bitamin/src/stories/organisms/Modal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f91b2c74fc0aa2bc9a9bcf5567e893358914818b55aefbb4530731c95addb708
-size 4155
+oid sha256:37f2cd0586224e9d8baf280675051878a87a6da48cadbb784153c3a6f3413402
+size 4152
