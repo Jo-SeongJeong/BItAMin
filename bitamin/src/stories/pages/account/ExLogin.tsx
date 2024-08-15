2551d1e38a6135c022a7913393490124e53a5611
최승탁 <tmdxkr5@hanmail.net>
2024-08-16 07:39:54 +0900

    refactor: 짜잘
    Former-commit-id: d380b05241fb59c5e7209645ef490da22ffd8e87

--- bitamin/src/stories/pages/account/ExLogin.tsx
+++ bitamin/src/stories/pages/account/ExLogin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e0471466285d58c17bdb6996e6836a24a4e0da37fc4c8b13eb827ca8f8be55d
-size 3919
+oid sha256:751a0749706027b174ac6aad8a25712ab422130e13ccf07ed9736045e900c99d
+size 3918
