d8f0608539c868897cefd9efb574861ba9e2190b
jaejunlee11 <sktks11@naver.com>
2024-08-15 18:19:47 +0900

    hotfix:build오류 제거
    Former-commit-id: cada8f62af0070e104fee1f905cdfff262659fbd

--- bitamin/src/router/AppRouter.tsx
+++ bitamin/src/router/AppRouter.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a3dfeb92aa8c0763d3c3998e30fb655406a4edcd34d4195bf174f442cff17fc
-size 4577
+oid sha256:b0972a1d3e55224b4c1574c7fd6d7e896f586e83e46b0da2bd5a1f615da03a4f
+size 4469
