36b036c45c8b53df917b424f73d0f01b7baeae3e
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 21:06:06 +0900

    hotfix: websocket Former-commit-id: 7f7c1e9e047720b15501070160660d3b67b8c03f

--- bitamin/src/api/WebSocketService.ts
+++ bitamin/src/api/WebSocketService.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d0bebb3b2502f55a2bdcf6adb08eb317a32aac5878cc8424e3756a056a299c5
-size 2005
+oid sha256:2f34085cdb3ac7eb20b55e70847f949f1670a77b46724dca8785b5c6ad2e41cd
+size 1591
