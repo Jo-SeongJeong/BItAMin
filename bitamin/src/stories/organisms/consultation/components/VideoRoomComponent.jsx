db677b56d726c7dcb29f10b3388b51c7c16ced5c
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 14:53:29 +0900

    refactor videoroom 및 sockjs install
    Former-commit-id: 6b8d2225e5cc72898a94a48c5d41007e6d81dff3

--- bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
+++ bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18527a8b4a46c4e67595f8c71c9cf4f58a83688145f032f70afd1b1cebd5111d
-size 24013
+oid sha256:84342d80951cfabccbc6909394a49f2f3bb299313651cd7bad3096fe87b7bc2a
+size 23537
