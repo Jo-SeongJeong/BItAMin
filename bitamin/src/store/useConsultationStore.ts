2bd6d319da8846f7ddc617a9149feaa8e1765f55
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 19:09:38 +0900

    hotfix: socket await Former-commit-id: 972d127791616a4252a8c49de45296b040d70ff2

--- bitamin/src/store/useConsultationStore.ts
+++ bitamin/src/store/useConsultationStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fe76ece23aab39474e3d9a199c82899db49e15ec45c2bd5673e8379b6f26751
-size 6929
+oid sha256:91e2cc6046eab9edff2d333b657b6252d86ba6966c9235fbbb13d87700045d6e
+size 6935
