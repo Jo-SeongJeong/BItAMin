9ae5ba5a09addc4e247002d087a71284df5e84e7
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 13:18:37 +0900

    refactor: 연속 stt 및 header 방 이동 후에도 마이페이지 로그아웃 보이던 오류 해결
    Former-commit-id: efe3b9b29bad1dac3b10d552ec820e24b5336a98

--- bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
+++ bitamin/src/stories/organisms/consultation/components/VideoRoomComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c37cb54f5892beba09af2b4e225c86dcbfd7b6e9f14c439655a854da14618be3
-size 22991
+oid sha256:18527a8b4a46c4e67595f8c71c9cf4f58a83688145f032f70afd1b1cebd5111d
+size 24013
