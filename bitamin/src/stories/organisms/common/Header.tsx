abe353e2edaa1520c8d0b9e8c8bcb8d54491e0e6
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 18:32:50 +0900

    hotfix: header Former-commit-id: de63ee8287bc139ff7d58085cdb398f433103fc2

--- bitamin/src/stories/organisms/common/Header.tsx
+++ bitamin/src/stories/organisms/common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25509044185f09a05e57b6989faf35885d46c69e8d6ad2ea97abae331114b906
-size 9292
+oid sha256:4bb9f1562d9a66821ee400b4bef5d1fab35fe5cf051a970ab484b8e340766a20
+size 9103
