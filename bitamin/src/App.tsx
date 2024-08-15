33f5be5dc10d77ddffc3e205b49ad7f384157fd7
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 12:06:55 +0900

    design: 메인페이지 전체화면
    Former-commit-id: 557157e06c4d4f392a6a99c43e3c66aad5f024ae

--- bitamin/src/App.tsx
+++ bitamin/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3335e14b8f772ff7ce6098c085900ca1c40bb1db2deabb082c9e4b3be501c41
-size 846
+oid sha256:9e23cea7d669aedb7ef7e6b5d278407593e6d698bc196cb17d4819e2124da6cc
+size 1095
