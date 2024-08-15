789838f50f0a2a6a60677412c9188a7f3d98fcb4
hyun <a94460093@gmail.com>
2024-08-15 16:47:00 +0900

    page: myplant 식물 레벨, 날씨 배경 추가
    Former-commit-id: c122428ed71de407cf2c864dc99eade740bcdca8

--- bitamin/src/router/AppRouter.tsx
+++ bitamin/src/router/AppRouter.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0af750c7472019f578ab8416f8ceb69de60e4b0c49ebba88ade1eb0322586c2
-size 4894
+oid sha256:e7af80eb7b44ca1ad44f4c4f0533898e61b3ef5c0733ebe4964174f8a21d4db6
+size 4728
