e23eb8dcbd8d07335504fdd2176bb5e4d1722d41
SeongJeong <jsj046@naver.com>
2024-08-16 09:32:23 +0900

    hotfix: 회원가입 시, 경험치 부여
    Former-commit-id: 29e819ff5878232c577a4b81ce145983c9c1a64b

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8438fc8f82af0a0e81dacb81c207e9c12c1da94edc3964d4b012f7632f6a3e6
-size 31772
+oid sha256:489d7e288dc03a233ace910e77a3a839ec8f1c4a9b15bfa34a75d1bb33cd3c45
+size 32130
