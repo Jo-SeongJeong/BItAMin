1bcca77cc7b96015570d66238d787bc6534ed7d8
이소연 <april_401@naver.com>
2024-08-16 11:25:13 +0900

    Update bitamin/src/stories/organisms/HospitalMap.tsx
    Former-commit-id: 223cc45d3a72ba3810fd6a3ef4bda4790fa1c1c6

--- bitamin/src/stories/organisms/HospitalMap.tsx
+++ bitamin/src/stories/organisms/HospitalMap.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f4b0ccb3b5bb8e267dfec01cbdb9c8ef469ec7a085fc394f9eb5814604fe943
-size 6449
+oid sha256:92ef6d25239d47e029b7896da93ab1780762719963dd3d986b3d637cff70bc51
+size 6409
