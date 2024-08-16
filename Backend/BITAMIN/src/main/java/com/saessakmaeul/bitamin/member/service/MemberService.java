906297a9a35f293fee9babf4ebfa1b017bcaa891
조성정 <jsj046@naver.com>
2024-08-16 10:10:29 +0900

    hotfix: 회원가입 시, 경험치 관리 테이블 인스턴스 생성 Former-commit-id: 20e49d2a9a1ced6ddbeea3cfe3d22252af7685b0

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:473f261740e6271a2a4da66c7d4dbe98ee21ad04456127cfe3a7b41230e03e85
-size 31773
+oid sha256:489d7e288dc03a233ace910e77a3a839ec8f1c4a9b15bfa34a75d1bb33cd3c45
+size 32130
--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/mission/entity/UserExperience.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/mission/entity/UserExperience.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Setter
 public class UserExperience {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
